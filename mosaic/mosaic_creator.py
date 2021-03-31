71d93ca22f8ae3193101fe62773b88e3e8573d31
E-m-o <vineet.biscuit@gmail.com>
2021-03-31 11:19:33 +0530

    Modified to create mosaics after calculation of indices

--- mosaic/mosaic_creator.py
+++ mosaic/mosaic_creator.py
@@ -3,76 +3,121 @@ from rasterio.merge import merge
 from rasterio.plot import show
 import glob
 import os
-import unzipper
+import matplotlib.pyplot as plt
 
 
-def mosaic_creator(down_dir_dict=dict, test=False):
+def mosaic_creator(base_path=None, test=False):
 
-    bands = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '8A']
+    print("================")
+    print("Creating Mosaics")
+    indices = ["ndvi", "ndwi", "ndmi", "savi", "msavi"]
+    satellite = None
+    # start_files = {}
+    # end_files = {}
+    files_dict = {}
+    date_list = ["Start_date", "End_date"]
 
-    start_files = {}
-    end_files = {}
-    base = os.getcwd()
-    # noinspection PyArgumentList
-    for (name, d_dir) in down_dir_dict.items():
-        path = os.path.join(d_dir, name)
-        if 'Start_date' in d_dir:
-            start_file = glob.glob(os.path.join(path, '**/*.TIF'), recursive=True)
-            start_file.sort()
-            if len(start_file) < 10:
-                start_file = glob.glob(os.path.join(path, '**/*.jp2'), recursive=True)
-            start_files[path] = start_file
-        if 'End_date' in d_dir:
-            end_file = glob.glob(os.path.join(path, '**/*.TIF'), recursive=True)
-            end_file.sort()
-            if len(end_file) < 10:
-                end_file = glob.glob(os.path.join(path, '**/*.jp2'), recursive=True)
-            end_files[path] = end_file
+    for date in date_list:
+        paths = glob.glob(os.path.join(base_path, "**/{}".format(date)), recursive=True)
+        path = paths[0]
+        print(path)
+        index_dict = {index: [] for index in indices}
+        for index in indices:
+            print("Creating index {} for {}".format(index, " ".join(date.split('_'))))
+            index_image = glob.glob("{}/**/*{}*".format(path, index), recursive=True)
+            print(index_image)
 
-    # print(start_files)
-    # print(end_files)
-    files_list = {'start': start_files,
-                  'end': end_files}
+            src_files_to_mosaic = []
+            for file in index_image:
+                src = rasterio.open(file)
+                src_files_to_mosaic.append(src)
+            print(src_files_to_mosaic)
 
-    for pos, files in files_list.items():
-        band_dict = {band: [] for band in bands}
-        for band in bands:
-            for key, value in files.items():
-                for val in value:
-                    if 'B{}.TIF'.format(band) in val:
-                        band_dict[band].append(val)
-                    if 'B{}.jp2'.format(band) in val:
-                        band_dict[band].append(val)
-                    elif 'B0{}.jp2'.format(band) in val:
-                        band_dict[band].append(val)
-
+            mosaic, out_trans = merge(src_files_to_mosaic)
+            # show(mosaic, cmap="gray")
+            # plt.waitforbuttonpress()
+            #
+            out_meta = src_files_to_mosaic[0].meta.copy()
+            out_meta.update({"driver": "GTiff",
+                             "height": mosaic.shape[1],
+                             "width": mosaic.shape[2],
+                             "transform": out_trans
+                             }
+                            )
+            #     # noinspection PyUnboundLocalVariable
+            #     split = "/".join(key.split("/")[:-1])
             try:
-                src_files_to_mosaic = []
-                for file in band_dict[band]:
-                    src = rasterio.open(file)
-                    src_files_to_mosaic.append(src)
-
-                mosaic, out_trans = merge(src_files_to_mosaic)
-                # show(mosaic, cmap='gray')
-
-                out_meta = src_files_to_mosaic[0].meta.copy()
-                out_meta.update({"driver": "GTiff",
-                                 "height": mosaic.shape[1],
-                                 "width": mosaic.shape[2],
-                                 "transform": out_trans
-                                 }
-                                )
-                # noinspection PyUnboundLocalVariable
-                os.chdir('/'.join(key.split('/')[:-1]))
+                try:
+                    os.chdir(os.path.join(path, 'mosaics'))
+                except FileNotFoundError:
+                    os.mkdir(os.path.join(path, 'mosaics'))
+                    os.chdir(os.path.join(path, 'mosaics'))
                 # print(os.getcwd())
-                with rasterio.open('{}_mosaic{}.tiff'.format(pos, band), "w", **out_meta) as destination:
+
+                pos = date.split('_')[0]
+                with rasterio.open("{}_mosaic_{}.tiff".format(date, index), "w",
+                                   **out_meta) as destination:
                     destination.write(mosaic)
-                os.chdir(base)
+                os.chdir(base_path)
             except IndexError:
                 pass
-        # print(band_dict)
+    # print(len(image_dir_list))
+    # for image_dir in image_dir_list:
+    #     print(image_dir)
+
+    # for date in date_list:
+    #     for image_dir in image_dir_list:
+    #         print(date, image_dir)
+    #         if date in image_dir:
+    #             files = glob.glob(os.path.join(image_dir, "/*.tiff"))
+    #             files.sort()
+    #             files_dict[image_dir] = files
+    #
+    # for key, value in files_dict.items():
+    #     print(key, value)
+
+    #     index_dict = {index: [] for index in indices}
+    #     for index in indices:
+    #         for key, value in files_dict.items():
+    #             for val in value:
+    #                 if "{}*.tiff".format(index) in val:
+    #                     index_dict[index].append(val)
+    #
+    #         try:
+    #             src_files_to_mosaic = []
+    #             for file in index_dict[index]:
+    #                 src = rasterio.open(file)
+    #                 src_files_to_mosaic.append(src)
+    #
+    #             mosaic, out_trans = merge(src_files_to_mosaic)
+    #             # show(mosaic, cmap="gray")
+    #
+    #             out_meta = src_files_to_mosaic[0].meta.copy()
+    #             out_meta.update({"driver": "GTiff",
+    #                              "height": mosaic.shape[1],
+    #                              "width": mosaic.shape[2],
+    #                              "transform": out_trans
+    #                              }
+    #                             )
+    #
+    #             # noinspection PyUnboundLocalVariable
+    #             split = "/".join(key.split("/")[:-1])
+    #             try:
+    #                 os.chdir(os.path.join(split, 'mosaics'))
+    #             except FileNotFoundError:
+    #                 os.mkdir(os.path.join(split, 'mosaics'))
+    #                 os.chdir(os.path.join(split, 'mosaics'))
+    #             # print(os.getcwd())
+    #
+    #             pos = date.split('_')[0]
+    #             with rasterio.open("{}_{}_mosaic{}.tiff".format(satellite, pos, index), "w",
+    #                                **out_meta) as destination:
+    #                 destination.write(mosaic)
+    #             os.chdir(base)
+    #         except IndexError:
+    #             pass
+    #     # print(index_dict)
+    # print("Mosaics created")
 
 
-# d = unzipper.decompress(sat_choice='1', path='/home/emo/Storage/Projects/Raster_Image_Calculator/Images')
-# # print(os.getcwd())
-# mosaic_creator(d)
+# d = unzipper.decompress(sat_choice="2", path="/home/emo/Storage/Projects/Raster_Image_Calculator/Images")
