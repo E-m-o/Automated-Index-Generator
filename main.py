443d15ed8154d37a223dbe2d6dd916a87ae74a7d
E-m-o <vineet.biscuit@gmail.com>
2021-04-01 11:42:34 +0530

    Split code into functions

--- main.py
+++ main.py
@@ -1,71 +1,83 @@
 from crawlers.web_crawler_earthexplorer import *
 from index_calculator.index_creator import *
+from mosaic.mosaic_creator import mosaic_creator
 from mosaic.unzipper import *
 from crawlers import web_crawler_earthexplorer as earth
 
 path_list = ['/home/emo/Storage/Projects/Raster_Image_Calculator/Test_files/test_mandala/mandlaBB.zip']
 # , '/home/emo/Storage/Projects/Raster_Image_Calculator/Test_files/test_mandala/mandlaBB-2.zip'
 
-earth.driver = make_driver()
+
+root = os.getcwd()
 choice = None
+
 request = input("Enter Request Number ->")
 base_path = '/home/emo/Storage/Projects/Raster_Image_Calculator/Images/Request.{}'.format(request)
-for path in path_list:
-    path_flag = False
-    while not path_flag:
-        if counter['login'] == 0:  # Login only once per session
-            # LOGIN PAGE ACCESSED
-            access_login()
 
-            # ENTERING LOGIN DETAILS
-            login()
-            counter['login'] = 1
 
-        if counter['explorer'] == 0:  # Access explorer only once per session
-            # driver.refresh()
+def downloader():
+    earth.driver = make_driver()
 
-            # ACCESS EXPLORER
-            access_explorer()
+    for path in path_list:
+        path_flag = False
+        while not path_flag:
+            if counter['login'] == 0:  # Login only once per session
+                # LOGIN PAGE ACCESSED
+                access_login()
 
-        # SELECT DATASET
-        choice = satellite_choice()
+                # ENTERING LOGIN DETAILS
+                login()
+                counter['login'] = 1
 
-        # GET DATES
-        date_list = get_dates(sat_choice=choice, test=False)
+            if counter['explorer'] == 0:  # Access explorer only once per session
+                # driver.refresh()
 
-        for date in date_list:
-            # FILE UPLOAD
-            upload_file(path)
+                # ACCESS EXPLORER
+                access_explorer()
 
-            # ADD DATES
-            add_date(date_list=date, test=False)
+            # SELECT DATASET
+            choice = satellite_choice()
 
-            # SET DATASET
-            set_dataset(sat_choice=choice)
+            # GET DATES
+            date_list = get_dates(sat_choice=choice, test=False)
 
-            # RESULTS
-            get_results()
+            for date in date_list:
+                # FILE UPLOAD
+                upload_file(path)
 
-            # GET METADATA
-            meta = get_metadata(sat_choice=choice)
+                # ADD DATES
+                add_date(date_list=date, test=False)
 
-            download_list = download_selector(meta, sat_choice=choice)
-            download(download_list, sat_choice=choice, test=False)
+                # SET DATASET
+                set_dataset(sat_choice=choice)
 
-        counter['explorer'] += 1
-        if counter['explorer'] == len(path_list):
-            if compare_flags():
-                print('Closing browser...')
-                # print(meta_dict)
-                time.sleep(10)
-                # driver.quit()
-                path_flag = True
-                break
-        else:
-            print(flags)
+                # RESULTS
+                get_results()
 
-down_dir, name = decompress(choice, base_path)
+                # GET METADATA
+                meta = get_metadata(sat_choice=choice)
 
-subdir_list = [x[0] for x in os.walk('.')]
-root = os.getcwd()
-crawler(subdir_list, root, test=False)
+                download_list = download_selector(meta, sat_choice=choice)
+                download(download_list, sat_choice=choice, test=False)
+
+            counter['explorer'] += 1
+            if counter['explorer'] == len(path_list):
+                if compare_flags():
+                    print('Waiting for downloads to complete...')
+                    # print(meta_dict)
+                    time.sleep(10)
+                    # driver.quit()
+                    path_flag = True
+                    break
+            else:
+                print(flags)
+
+
+def processor():
+    down_dir_dict = decompress(base_path)
+    generate_indices(down_dir_dict, test=False)
+    mosaic_creator(base_path)
+
+
+# downloader()
+processor()
